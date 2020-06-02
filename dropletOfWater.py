57a635175ce45e44cc6fa364cc0fbd5945a4a20c
Pawn <48128500+ColourlessPawn@users.noreply.github.com>
2020-06-02 17:36:40 +0200

    Just fixing some style problems
    Trying to do the simple code as simple as it should be

--- dropletOfWater.py
+++ dropletOfWater.py
@@ -2,53 +2,45 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-##
+ROWS = 12
+COLUMNS = 11
+
+
+# Starting the algorithm
 def flood(arena, pos_x, pos_y):
     # We're gonna flood the arena
-    flood_spot(arena, pos_x, pos_y, 0, False)
+    flood_spot(arena, pos_x, pos_y, 0)
 
 
-def flood_spot(arena, pos_x, pos_y, iteration, resuelto):
+def flood_spot(arena, pos_x, pos_y, iteration):
     value = iteration + 1
 
-    if 0 <= pos_x < 7 and 0 <= pos_y < 7 and (
-            stage[pos_x][pos_y] >= value or stage[pos_x][pos_y] == 0) and not resuelto:
+    if 0 <= pos_x < ROWS and 0 <= pos_y < COLUMNS and (
+            stage[pos_x][pos_y] >= value or stage[pos_x][pos_y] == 0):
         # Let's flood the spot
         iteration = iteration + 1
-        if stage[pos_x][pos_y] == 99:
-            resuelto = True
-            print(arena)
+        if stage[pos_x][pos_y] == 999:
             return
 
         stage[pos_x][pos_y] = iteration
         # Let's continue flooding
-        flood_spot(arena, pos_x + 1, pos_y, iteration, resuelto)
-        flood_spot(arena, pos_x, pos_y + 1, iteration, resuelto)
-        flood_spot(arena, pos_x - 1, pos_y, iteration, resuelto)
-        flood_spot(arena, pos_x, pos_y - 1, iteration, resuelto)
-        print(arena)
+        flood_spot(arena, pos_x + 1, pos_y, iteration)
+        flood_spot(arena, pos_x, pos_y + 1, iteration)
+        flood_spot(arena, pos_x - 1, pos_y, iteration)
+        flood_spot(arena, pos_x, pos_y - 1, iteration)
 
-    else:
-        print("fin")
-
-
-# Let's have a given values:
-rows = 7
-columns = 7
-number_of_stones = 12
 
 # We're gonna act with this as a matrix.
-stage = np.random.randint(1, size=(rows, columns))
+stage = np.random.randint(1, size=(ROWS, COLUMNS))
 
 # And we're gonna place two mans.
-stage[1][1] = 99
+stage[1][1] = 999
 print(stage)
-flood(stage, 1, 5)
+flood(stage, 1, 6)
+flood(stage, 8, 2)
+stage[1][1] = 0
 
-stage[1][1]=0
-##Printeamos:
+# Printing the graph.
 fig, ax = plt.subplots()
-
-ax.matshow(stage, cmap=plt.cm.Blues)
-
+ax.matshow(stage, cmap=plt.cm.Greens)
 plt.show()
